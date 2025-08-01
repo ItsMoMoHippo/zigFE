67dfd3e75b2aef6dca8a3e47e403c631409b133d
Momo <pr0h3ckr@gmail.com>
2025-08-01 17:53:57 +0100

    reading all entries in a dir

--- src/main.zig
+++ src/main.zig
@@ -1,32 +1,48 @@
 const std = @import("std");
+const fs = std.fs;
+const print = std.debug.print;
 
 pub fn main() !void {
-    std.debug.print("Hello world!\n", .{});
-
-    std.debug.print("\n", .{});
-
-    //const array
-    const words = [_][]const u8{ "apple", "banana", "pear" };
-
-    for (words) |word| {
-        std.debug.print("{s}\n", .{word});
-    }
-
-    std.debug.print("\n", .{});
-
-    //array list
+    //allocator
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
-    var list = std.ArrayList([]const u8).init(alloc);
-    defer list.deinit();
+    //open up dir
+    var cwd = try fs.cwd().openDir(".", .{ .iterate = true });
+    defer cwd.close();
 
-    try list.append("orange");
-    try list.append("grape");
-    try list.append("mango");
+    //make lists
+    var dirs = std.ArrayList([]const u8).init(alloc);
+    defer dirs.deinit();
+    var files = std.ArrayList([]const u8).init(alloc);
+    defer files.deinit();
 
-    for (list.items) |item| {
-        std.debug.print("{s}\n", .{item});
+    //iterate over dir
+    var iter = cwd.iterate();
+    while (try iter.next()) |entry| {
+        switch (entry.kind) {
+            .directory => try dirs.append(entry.name),
+            .file => try files.append(entry.name),
+            else => {},
+        }
     }
+
+    //sort alphabetically
+    std.mem.sortUnstable([]const u8, dirs.items, {}, sortSlices);
+    std.mem.sortUnstable([]const u8, files.items, {}, sortSlices);
+
+    //print
+    print("dirs:\n", .{});
+    for (dirs.items) |dir| {
+        print("\t{s}/\n", .{dir});
+    }
+    print("files:\n", .{});
+    for (files.items) |file| {
+        print("\t{s}\n", .{file});
+    }
+}
+
+fn sortSlices(_: void, a: []const u8, b: []const u8) bool {
+    return std.mem.lessThan(u8, a, b);
 }
