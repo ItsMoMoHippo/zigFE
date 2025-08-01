a66741d24f6d10cb8da4dc4622e44e62b6fe9b7f
Momo <pr0h3ckr@gmail.com>
2025-08-01 12:06:20 +0100

    arrays and arraylists

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,32 @@
 const std = @import("std");
 
 pub fn main() !void {
-    std.debug.print("Hello world!", .{});
-}
+    std.debug.print("Hello world!\n", .{});
 
+    std.debug.print("\n", .{});
+
+    //const array
+    const words = [_][]const u8{ "apple", "banana", "pear" };
+
+    for (words) |word| {
+        std.debug.print("{s}\n", .{word});
+    }
+
+    std.debug.print("\n", .{});
+
+    //array list
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const alloc = gpa.allocator();
+
+    var list = std.ArrayList([]const u8).init(alloc);
+    defer list.deinit();
+
+    try list.append("orange");
+    try list.append("grape");
+    try list.append("mango");
+
+    for (list.items) |item| {
+        std.debug.print("{s}\n", .{item});
+    }
+}
